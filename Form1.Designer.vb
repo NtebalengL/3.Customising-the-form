437cc9b0c1373f709c55b29f739b676d2b309ead
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-04 00:42:26 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnClose = New Button()
+        btnLebo = New Button()
+        SuspendLayout()
+        ' 
+        ' btnClose
+        ' 
+        btnClose.Location = New Point(305, 144)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(112, 34)
+        btnClose.TabIndex = 0
+        btnClose.Text = "Close"
+        btnClose.UseVisualStyleBackColor = True
+        ' 
+        ' btnLebo
+        ' 
+        btnLebo.Location = New Point(176, 82)
+        btnLebo.Name = "btnLebo"
+        btnLebo.Size = New Size(112, 34)
+        btnLebo.TabIndex = 1
+        btnLebo.Text = "Lebo"
+        btnLebo.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnLebo)
+        Controls.Add(btnClose)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnLebo As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "My first VB App"
+        Me.BackColor = Color.DeepPink
+        Me.MaximizeBox = False
+        Me.MinimizeBox = True
+        Me.Size = New Size(750, 750)
+        Me.Opacity = 0.89        'Makes the box a bit transparent
+        Me.CenterToParent()     'Centers the exec box to the centre of the screen
+        Me.CenterToScreen()
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+
+    End Sub
+
+    Private Sub btnLebo_Click(sender As Object, e As EventArgs) Handles btnLebo.Click
+        MsgBox("This is Lebo",, "Hello")
+    End Sub
+End Class
